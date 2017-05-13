5d8032cd8ce3801610d2b30077ef1c1c842bec23
James Dooley <james.dooley@bbc.co.uk>
2017-05-13 19:00:58 +0100

    Fixing annotate script

--- scripts/annotate-coments.js
+++ scripts/annotate-coments.js
@@ -3,22 +3,41 @@
 const fs = require('fs');
 const languageClient = require('../src/language-client');
 
+function mapComment(comment, annotations) {
+  return {
+    message: comment.message,
+    id: comment.id,
+    assetId: comment.assetId,
+    assetUrl: comment.assetUrl,
+    assetHeadline: comment.assetHeadline,
+    voteRating: comment.voteRating,
+    voteCount: comment.voteCount,
+    // replies: mapComment() : TODO: replies
+    entities: annotations.entities,
+    sentiment: annotations.sentiment,
+    sentences: annotations.sentences
+  }
+}
+
 module.exports = (comments) => {
   const annotatedData = [];
+  let count = 0;
 
   comments.forEach((comment) => {
-    languageClient.annotate(comment, (err, results) => {
+    languageClient.annotate(comment, (err, annotations) => {
       if (err) {
         console.log(err);
       } else {
-        results.original_text = comment.message;
-        annotatedData.push(results);
+        annotatedData.push(mapComment(comment, annotations));
+      }
+
+      count++;
+      if (count === comments.length) {
+        fs.writeFileSync('test/fixtures/annotated-comments.json', JSON.stringify(annotatedData));
       }
     });
   });
-
-  fs.writeFileSync('test/fixtures/annotated-comments.json', JSON.parse(annotatedData));
 };
 
-// const comments = require('./test/fixtures/top-comments-4493596');
-// module.exports(comments);
+const comments = require('../test/fixtures/top-comments-4493596');
+module.exports(comments);
